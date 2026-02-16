a75aaddb58a32bb11f829cb33f14b34b5d39ad5f
hbbell <hbbell@mtu.edu>
2026-02-16 18:36:01 -0500

    Commit 3

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 // Commit 1 - Initial version
-public class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub Actions");
     }
