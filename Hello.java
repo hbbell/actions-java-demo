f6907056327d0e4272b9cc2cb9d97f5184b4ec16
hbbell <hbbell@mtu.edu>
2026-02-16 18:18:25 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello {
+public class HelloWorld {  // Class name doesn't match filename
     public static void main(String[] args) {
-        System.out.println("Hello from GitHub Actions");
+        System.out.println("Hello World");
     }
 }
